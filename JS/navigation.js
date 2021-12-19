c810b2ba7bdcbb66157a2a09b06e1a95b8647ad5
Giga <60932604+Gkhundadze@users.noreply.github.com>
2021-12-19 23:37:24 +0400

    added menu modal
    on menu click show modal with main menu

--- JS/navigation.js
+++ JS/navigation.js
@@ -1,5 +1,20 @@
 const navigation = document.getElementById('navigation');
+// modal variables
+const menuModal = document.querySelector('.menu-modal-wrapper');
+const modalCloseButton = document.querySelector('.close-menu-button');
+const mainMenuButton = document.querySelector('.main-menu-button');
 
 setTimeout(() => {
   navigation.style.display = 'block';
-}, 1700);
+}, 700);
+
+mainMenuButton.addEventListener('click', (e) => {
+  menuModal.classList.add('menu-active');
+  modalCloseButton.style.pointerEvents = 'all'
+})
+
+modalCloseButton.addEventListener('click', () => {
+  if (menuModal.classList.contains("menu-active")) {
+    menuModal.classList.remove("menu-active");
+  }
+})
